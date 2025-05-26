6951a4b054f584c90bc7954f52e31452fdbf525a
Vergusha <Vergillyi@outlook.com>
2025-05-26 19:55:51 +0200

    Проблемы с генерацией чанков[#2]

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -6,11 +6,18 @@ public class PlayerMovement : MonoBehaviour
     //Movement
         public float moveSpeed;
         Rigidbody2D rb;
-        Vector3 moveDir;
+        
+        [HideInInspector]
+        public float LastHorizontalVector;
+        [HideInInspector]
+        public float LastVerticalVector;
+        [HideInInspector]
+        public Vector3 moveDir;
+        
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
@@ -29,11 +36,20 @@ public class PlayerMovement : MonoBehaviour
         float moveX = Input.GetAxisRaw("Horizontal");
         float moveY = Input.GetAxisRaw("Vertical");
 
-        moveDir = new Vector3(moveX, moveY).normalized;
+        moveDir = new Vector2(moveX, moveY).normalized;
+
+        if (moveDir.x != 0)
+        {
+            LastHorizontalVector = moveDir.x;
+        }
+        if (moveDir.y != 0)
+        {
+            LastVerticalVector = moveDir.y;
+        }
     }
 
     void Move()
     {
-        rb.linearVelocity = new Vector2(moveDir.x * moveSpeed, moveDir.y * moveSpeed);
+        rb.linearVelocity = new Vector3(moveDir.x * moveSpeed, moveDir.y * moveSpeed);
     }
 }
